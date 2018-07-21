97d3fe93e7286e7cc5b8a2aac1b08c72bffbcb06
Yuhell <kevin.dintanavong@hotmail.fr>
2018-07-21 20:35:25 +0200

    Favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link rel="icon" href="{{ asset('img/logo.jpg') }}"/>
         @yield('specific-css')
     </head>
 <body>
